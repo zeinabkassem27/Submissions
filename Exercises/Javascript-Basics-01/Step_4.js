06c58ec4b3cdbcbc76d329f3b2a32489776740a0
zeinabkassem27 <zeinab_kassem@hotmail.com>
2019-12-16 13:50:39 +0200

    js-4

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,12 @@
+function v() {
+  var name;
+  alert();
+  alert(
+    "nom: " +
+      document.getElementById("name").value +
+      "\nPrenom: " +
+      document.getElementById("surname").value +
+      "\nVille: " +
+      document.getElementById("city").value
+  );
+}
