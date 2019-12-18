8597fb1dd29742e29c1df81522b7ae3ee0802ea5
zeinabkassem27 <zeinab_kassem@hotmail.com>
2019-12-18 10:29:25 +0200

    basic3-step3

---
+++ Exercises/Javascript-Basics-03/step 3/step3.js
@@ -0,0 +1,14 @@
+function myFunc(x) {
+  let y = document.getElementById("text");
+  switch (x) {
+    case "green":
+      y.style.color = "green";
+      break;
+    case "red":
+      y.style.color = "red";
+      break;
+    case "blue":
+      y.style.color = "blue";
+      break;
+  }
+}
