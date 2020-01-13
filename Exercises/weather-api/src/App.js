97914fc9b4f1675fc6ee7ad1b56da6582e8977c7
Gaby Karam <gaby@codi.tech>
2020-01-13 09:16:04 +0200

    adding weather api exercise

---
+++ Exercises/weather-api/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header"></header>
+        <main className="app__main"></main>
+      </div>
+    );
+  }
+}
+
+export default App;
