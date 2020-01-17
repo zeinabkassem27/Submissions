1a1a559e7cf243289356e265bbbca3036dd26a20
zeinabkassem27 <zeinab_kassem@hotmail.com>
2020-01-17 16:26:44 +0200

    basic queries

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,7 @@
+select name from students
+select * from students where Age>30
+select name from students where Age>30 and Gender>30
+select Points from students where Name='Alex'
+insert	into students values (null, 'Zeinab', 27, 'F', 200)
+UPDATE students SET Points = 325 WHERE Name='Basma';
+UPDATE studentsSET Points = 180 WHERE Name='Alex'; 
