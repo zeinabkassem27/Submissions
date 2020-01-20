a62d24a97e084ebbb920433e37528a4a1bf2b2f5
zeinabkassem27 <zeinab_kassem@hotmail.com>
2020-01-20 09:47:27 +0200

    changing name in startApp function

--- Challenge/NodeJS/tasks.js
+++ Challenge/NodeJS/tasks.js
@@ -1,50 +1,45 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", onDataReceived);
+  console.log(`Welcome to ${name}'s application!`);
+  console.log("--------------------");
 }
-
 
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n') {
+  if (text === "quit") {
     quit();
-  }
-  else if(text === 'hello\n'){
+  } else if (text === "hello") {
     hello();
-  }
-  else{
+  } else {
     unknownCommand(text);
   }
 }
-
 
 /**
  * prints "unknown command"
@@ -53,30 +48,28 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"');
 }
-
 
 /**
  * Says hello
  *
  * @returns {void}
  */
-function hello(){
-  console.log('hello!')
+function hello() {
+  console.log("hello!");
 }
-
 
 /**
  * Exits the application
  *
  * @returns {void}
  */
-function quit(){
-  console.log('Quitting now, goodbye!')
+function quit() {
+  console.log("Quitting now, goodbye!");
   process.exit();
 }
 
 // The following line starts the application
-startApp("Jad Sarout")
+startApp("Zeinab kassem");
